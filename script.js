5fc1b5596c57c5932ae3fda6620c9f6934c57f68
ucryys-hash <ucryys@gmail.com>
2026-07-06 16:59:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 // Premium Script
+document.getElementById("app").innerHTML = `
+<div class="card">
+    <img src="nina.jpg" alt="Nina">
+
+    <h1>Hey Nina ❤️</h1>
+
+    <p>
+        I made something just for you.<br><br>
+        Click the button below...
+    </p>
+
+    <button id="openBtn">
+        Open Surprise ✨
+    </button>
+</div>
+`;
+
+document.getElementById("openBtn").onclick = () => {
+    alert("This is just the beginning ❤️");
+};
